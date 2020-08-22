665523b144ff02f1d5a980aea042585742a6b3c9
Kamil Wawrzak <kamwawrzak@gmail.com>
2020-08-22 16:12:30 +0200

    initialize flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index_page():
+    return 'Welcome to the bar application.'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
