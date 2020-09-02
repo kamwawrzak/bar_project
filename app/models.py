357f7f5225afbfad2b56c0f27cefe8b5d8a30ebc
Kamil Wawrzak <kamwawrzak@gmail.com>
2020-09-02 17:29:47 +0200

    Add empty drink model

--- app/models.py
+++ app/models.py
@@ -5,3 +5,7 @@ class RegularUser(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(86), nullable=False)
+
+
+class Drink(db.Model):
+    pass
