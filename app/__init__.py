7d3c803f5801911537476b02b4eaf6d557a7755f
Kamil Wawrzak <kamwawrzak@gmail.com>
2020-09-08 20:30:43 +0200

    Add user login functionallity

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
 from flask import Flask
 
+from flask_login import LoginManager
+
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+lm = LoginManager()
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config.Config')
     db.init_app(app)
+    lm.init_app(app)
 
     with app.app_context():
         from . import routes  # noqa
-        from .blueprints import register_bp
+        from .blueprints import login_bp, register_bp
+        app.register_blueprint(login_bp.login_bp)
         app.register_blueprint(register_bp.register_bp)
         db.create_all()
         return app
