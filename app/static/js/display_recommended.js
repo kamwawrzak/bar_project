eb9af01ce31497ada9733481a4eaed0d24296443
Kamil Wawrzak <kamwawrzak@gmail.com>
2020-12-30 09:09:50 +0100

    Fix few small issues

--- app/static/js/display_recommended.js
+++ app/static/js/display_recommended.js
@@ -2,14 +2,14 @@ $(window).on('load', function(){ getRecommended(); });
 
 function getRecommended(){
     $.getJSON('/v1/most_viewed', function (data){
-        $('#most-viewed-name').html(data.name);
+        $('#most-viewed-name').html(data.name.capitalize());
         $('a[href="/most-viewed-drink"]').each(function(){
             this.href = this.href.replace('/most-viewed-drink', '/v1/drink/' + data.id);})
         $('#most-viewed-img').attr('src', data.image);
     });
 
     $.getJSON('/v1/top_rated', function (data){
-        $('#top-rated-name').html(data.name);
+        $('#top-rated-name').html(data.name.capitalize());
         $('a[href="/top-rated-drink"]').each(function(){
             this.href = this.href.replace('/top-rated-drink', '/v1/drink/' + data.id);})
         $('#top-rated-img').attr('src', data.image);
